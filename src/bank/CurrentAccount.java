158e1541535db91fc97a4b31a7e048b7540eae01
Kwame Oduro <odurokwameee@gmail.com>
2025-04-10 16:59:35 +0000

    add: Current Account

---
+++ src/bank/CurrentAccount.java
@@ -0,0 +1,34 @@
+package bank;
+
+public class CurrentAccount extends BankAccount implements BankOperations {
+    private double overdraftLimit;
+
+    public CurrentAccount(String accountNumber, double balance, double overdraftLimit) {
+        super(accountNumber, balance);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (balance - amount >= -overdraftLimit) {
+            balance -= amount;
+        } else {
+            System.out.println("Please your overdraft limit is exceeded");
+        }
+    }
+
+    @Override
+    public double checkBalance() {
+        return balance;
+    }
+
+    // Getter for overdraft limit
+    public double getOverdraftLimit() {
+        return overdraftLimit;
+    }
+}
